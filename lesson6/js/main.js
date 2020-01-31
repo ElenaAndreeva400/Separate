63114479d8e9b7c178985274ebb579ba8e4f910b
Elenandreeva <confeta@bk.ru>
2020-01-31 22:56:19 +0300

    +

--- lesson6/js/main.js
+++ lesson6/js/main.js
@@ -7,13 +7,14 @@ let isNumber = function(num) {
 function gameBot() {
 
   let target = 20;
+  //Math.floor(Math.random() * (100 - 1)) + 1;
 
   let numbers = +prompt('Угадай число от 1 до 100');
   console.log(numbers);
  
-        if (numbers < 0 || numbers > 100) {
-        confirm('Отрицательные числа и числа больше 100 не принимаются. Введите еще один вариант');
-        gameBot();
+    if (numbers < 0 || numbers > 100) {
+    confirm('Отрицательные числа и числа больше 100 не принимаются. Введите еще один вариант');
+    gameBot();
       } else if(numbers > target) {
         confirm('Загаданное число меньше. Введите еще один вариант');
         gameBot();
@@ -24,16 +25,15 @@ function gameBot() {
         confirm('Это не число! Введите новое число.');
         gameBot();
       } else if(numbers === target) {
-        confirm('Поздравляем, Вы отгадали! Продолжаем?');
-            if (!confirm()) {
-            return;
-          } else {
+        let answer = confirm('Поздравляем, Вы отгадали!');
+            if (answer) {
             gameBot();
-          } 
-      } else if (!confirm()) {
-        return;
-      }
-  
+            } else {
+              return;
+            } 
+      // } else if (!confirm()) {
+      //   return;
+    }
 }
 
 gameBot();
